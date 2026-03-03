194d0e85ddbc11f5403a3d8da05ff3aff6d4db4b
Tosin-ultimate <ibitoyetosin12@gmail.com>
2026-03-03 22:09:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+ const CACHE_NAME = "gentletech-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/logo.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.error("Failed to cache resources:", err);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
